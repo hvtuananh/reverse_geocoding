3aae44815051025a2f45bf709ee9c0ceb8cccfd6
tuananh <hvtuananh@gmail.com>
2014-06-17 14:45:23 -0400

    add script to build tree

---
+++ kdtree.py
@@ -0,0 +1,10 @@
+class KdNode:
+    def __init__(self):
+        self.data = None
+        self.depth = 0
+        self.pivot = 0
+        self.left = None
+        self.right = None
+        pass
+        
+    
