a3b07771acd75b974bd2287deaee798302e134e1
tuananh <hvtuananh@gmail.com>
2014-06-18 15:57:27 -0400

    test geocoder

--- test_geocoder.py
+++ test_geocoder.py
@@ -1,6 +1,16 @@
 import timeit
+from geocoder import Geocoder
+g = Geocoder('manhattan-addresses.bin', 'manhattan-kdtree.bin')
+
+print g.address_to_latlng('10 WEST 49 STREET')
+print g.address_to_latlng('10 WEST 49 ST')
+print g.address_to_latlng('10 W 49 ST')
+
 
 print timeit.timeit("""g.address_to_latlng('99 BROADWAY')""",setup="""from geocoder import Geocoder
 g = Geocoder('manhattan-addresses.bin', 'manhattan-kdtree.bin')""")
+
+'''
 print timeit.timeit("""g.latlng_to_address((40.769083827890107,-73.952477804528087))""",setup="""from geocoder import Geocoder
 g = Geocoder('manhattan-addresses.bin', 'manhattan-kdtree.bin')""")
+'''
